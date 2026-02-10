67f7d231186cb87b4f7abe8a645e28889d62526c
shin2721 <shin2721@users.noreply.github.com>
2026-02-10 20:29:26 +0900

    test(jest): mock expo-constants for CJS runtime

--- psycle-expo/jest.setup.js
+++ psycle-expo/jest.setup.js
@@ -4,3 +4,13 @@
 jest.mock('@react-native-async-storage/async-storage', () =>
   require('@react-native-async-storage/async-storage/jest/async-storage-mock')
 );
+
+// expo-constants is ESM; mock it in Jest's CJS runtime.
+jest.mock('expo-constants', () => ({
+  __esModule: true,
+  default: {
+    expoConfig: {
+      extra: {},
+    },
+  },
+}));
